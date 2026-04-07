29a367e20dc66ece260d0dffc984f467dfc2d3fe
Patricio Arbulo <pa287194@fi365.ort.edu.uy>
2026-04-07 15:26:22 -0300

    Added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
